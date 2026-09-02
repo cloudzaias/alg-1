a2d5ba845f89f7c9fb27e1b1d523eea3bcc12efd
Izaias Santos <izaiaspcl.santos@gmail.com>
2026-09-02 11:04:02 -0300

    Add files via upload

---
+++ EX1_2.java
@@ -0,0 +1,20 @@
+public class EX1_2{
+
+    public static void main(String[] args) {
+        System.out.println("Nome: Izaias Periera Da Cruz Lopes Santos");
+        System.out.println("R.A:" + 6270179);
+        System.out.println("Curso: Analise e desenvolvimento de sistemas.");
+
+
+// Exercicio 2
+        System.out.println("--------------------------------------");
+        System.out.println("Idade:" + 22);
+        System.out.println("Altura:" + 1.68);
+        System.out.println("Presente");
+                        
+
+    }
+}
+
+
+
